0f60a6c75562d363374b70b0f0f6c1132a8958b1
abhishek kumar <abhisingh2084@gmail.com>
2024-05-29 17:53:31 +0530

    Add project files.

---
+++ LoginFormsASPCore/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using LoginFormsASPCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace LoginFormsASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CodeFirstDbContext context;
+
+        public HomeController(CodeFirstDbContext context)
+        {
+            this.context = context;
+        }
+         
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult lOGIN()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult lOGIN(UserTbl user)
+        {
+             
+            var myUser =  context.UserTbls.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+            if (myUser != null)
+            {
+                HttpContext.Session.SetString("UserSession",myUser.Email);
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed";
+            }
+            return View(myUser);
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("LOGIN");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                 HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("LOGIN");
+            }
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoginFormsASPCore/Models/UserTbl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginFormsASPCore.Models
+{
+    public partial class UserTbl
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public int Age { get; set; }
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
